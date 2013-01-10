92a0d270267f2c8d98f42f26f4e9d3c514fa4322
Vince Weaver <vincent.weaver@maine.edu>
2013-01-10 14:49:31 -0500

    65c816: update conversion tool
    now it includes linefeeds which are needed for linux_logo

--- no-os/65c816_snes/tools/string_to_bytes.c
+++ no-os/65c816_snes/tools/string_to_bytes.c
@@ -2,20 +2,21 @@
 #include <string.h>
 
 int main(int argc, char **argv) {
-   
+
    char buffer[BUFSIZ],*result;
    int i;
    int quotes_open=0;
-	       
+
    while(1) {
       result=fgets(buffer,BUFSIZ,stdin);
       if (result==NULL) return 0;
-      
+
       printf("\t.byte ");
-      for(i=0;i<strlen(buffer)-1;i++) {
+      for(i=0;i<strlen(buffer);i++) {
 	 if (buffer[i] < 32) {
 	    if (quotes_open) printf("\",");
-	    printf("$%2x, ",buffer[i]);
+	    printf("$%02x",buffer[i]);
+	    if (i<strlen(buffer)-1) printf(", ");
 	    quotes_open=0;
 	 }
 	 else {
@@ -27,9 +28,8 @@ int main(int argc, char **argv) {
       if (quotes_open) printf("\"");
       quotes_open=0;
       printf("\n");
-    
-      
+
    }
- 
+
    return 0;
 }
