22f0b2dd8ab8261d1744cb39103a1671490cf5f5
Vince Weaver <vincent.weaver@maine.edu>
2013-10-22 15:23:00 -0400

    vax: clean up the comments a bit
    also remove extraneous whitespace

--- ll.vax.s
+++ ll.vax.s
@@ -1,8 +1,11 @@
 #
-#  linux_logo in vax assembler 0.29
+#  linux_logo in vax assembler 0.48
 #
-#  Originally by 
+#  By:
 #       Vince Weaver <vince _at_ deater.net>
+#
+#  Further optimization by:
+#       Anders Magnusson <ragge _at_ ludd.ltu.se>
 #
 #  assemble with     "as -o ll.o ll.vax.s"
 #  link with         "ld -o ll ll.o"
@@ -47,8 +50,6 @@
 
 # cannot use PC or SP with instructions that group more than one reg together
 
-
-
 #
 # Syscalls are complicated, they are treated like a function call
 #    save the ap register (argument pointer reg)
@@ -66,102 +67,101 @@
 #   which says which regs to save/restore.  If you don't get this right
 #   weird errors will happen.
 
-
-# Integer opcodes
-# adawi - add aligned word interlock (locking)
-# addb2,addb3,addw2,addw3,addl2,addl3 (2 and 3 operation adds of var sizes)
-# adwc - add with carry
-# ashl,ashq - arithmetic shift.  Shift negative for left, pos for right
-# bicb2,bicb3,bicw2,bicw3,bicl2,bicl3 - bit clear
-# bisb2,bisb3,bisw2,bisw3,bisl2,bisl3 - bit set
-# bitb,bitw,bitl - bit test
-# clrb,clrw,clrl,clrq,clro - clear reg to 0 (1 byte shorter than a move)
-# cmpb,cmpw,cmpl - compare
-# cvtbw,cvtbl,cvtwb,cvtwl.cvtlb,cvtlw - convert type.  sign-extends or truncs
-# decb,decw,decl - decrement.  (1 byte shorter than subtract)
-# divb2,divb3,divw2,divw3,divl2,divl3 - divide
-# ediv - extended divide (with remainder)
-# emul - extended multiply (with 64-bit result)
-# incb,ibcw,incl - increment (1 byte shorter than an add)
-# mcomb,mcomw,mcoml - move complement
-# mnegb,mnegw,mnwgl - move negated
-# movb,movw,movl,movq,movo - move (updates flags)
-# movzbw,movzbl,movzwl - move zero extended
-# mulb2,mulb3,mulw2,mulw3,mull2,mull3 - multiply
-# pushl - push long (equiv to movl src, -(sp) but one byte shorter)
-# rotl - rotate long 
-# sbwc - subtract with carry
-# subb2,subb3,subw2,subw3,subl2,subl3 - subtract
-# tstb,tstl,tstw - test (equiv to compare with 0, but one byte shorter)
-# xorb2,xorb3,xorw2,xorw3,xorl2,xorl3 - xor
+# Integer opcodes:
+# + adawi - add aligned word interlock (locking)
+# + addb2,addb3,addw2,addw3,addl2,addl3 (2 and 3 operation adds of var sizes)
+# + adwc - add with carry
+# + ashl,ashq - arithmetic shift.  Shift negative for left, pos for right
+# + bicb2,bicb3,bicw2,bicw3,bicl2,bicl3 - bit clear
+# + bisb2,bisb3,bisw2,bisw3,bisl2,bisl3 - bit set
+# + bitb,bitw,bitl - bit test
+# + clrb,clrw,clrl,clrq,clro - clear reg to 0 (1 byte shorter than a move)
+# + cmpb,cmpw,cmpl - compare
+# + cvtbw,cvtbl,cvtwb,cvtwl.cvtlb,cvtlw - convert type.  sign-extends or truncs
+# + decb,decw,decl - decrement.  (1 byte shorter than subtract)
+# + divb2,divb3,divw2,divw3,divl2,divl3 - divide
+# + ediv - extended divide (with remainder)
+# + emul - extended multiply (with 64-bit result)
+# + incb,ibcw,incl - increment (1 byte shorter than an add)
+# + mcomb,mcomw,mcoml - move complement
+# + mnegb,mnegw,mnwgl - move negated
+# + movb,movw,movl,movq,movo - move (updates flags)
+# + movzbw,movzbl,movzwl - move zero extended
+# + mulb2,mulb3,mulw2,mulw3,mull2,mull3 - multiply
+# + pushl - push long (equiv to movl src, -(sp) but one byte shorter)
+# + rotl - rotate long
+# + sbwc - subtract with carry
+# + subb2,subb3,subw2,subw3,subl2,subl3 - subtract
+# + tstb,tstl,tstw - test (equiv to compare with 0, but one byte shorter)
+# + xorb2,xorb3,xorw2,xorw3,xorl2,xorl3 - xor
 
 # address opcodes
-# movab,movaw,moval,movaf,movaq,movad,movag,movah,movao - move address ?
-# pushab,pushaw,pushal,pushaf,pushaq,pushad,pushag,pushah,pushao - push addy
+# + movab,movaw,moval,movaf,movaq,movad,movag,movah,movao - move address ?
+# + pushab,pushaw,pushal,pushaf,pushaq,pushad,pushag,pushah,pushao - push addy
 
 # variable bitlength opcodes
-# cmp,ext,ff,insv
-# - i leave these out, gas can't assemble them
+# + cmp,ext,ff,insv
+#   - i leave these out, gas can't assemble them
 
 # control instructions
-# acbb,acbw,acbl,acbf,acbd,acbg,acbh - add compare and branch 
-# aobleq - add one and branch less than or equal
-# aoblss - add one and branch less than
-# bgtr,bleq,bneq,bnequ,beql,beqlu,bgeq,blss,bgtru,blequ
-# bvc,bvs,bgequ,bcc,blssu,bcs - various branches, signed and unsigned
-# bbs,bbc - branch on bit set/clea
-# bbss,bbcs,bbsc,bbcc - branch on bit and set/clear
-# bbssi,bbcci - branch on bit set and set/clear interlocked
-# blbs, blbc - branch on low bit set/clear
-# brb,brw - branch
-# bsbb,bsbw - branch to subroutine.  PC is pushed on stack
-# caseb,casew,casel - case statements in hardware
-# jmp - jump
-# jsb - jump to subroutine
-# rsb - return from subroutine (equiv to jmp @(sp)+ but 1 byte shorter)
-# sobgeq - subtract one and branch greater than or equal
-# sobgtr - subtract one and branch greater than
+# + acbb,acbw,acbl,acbf,acbd,acbg,acbh - add compare and branch
+# + aobleq - add one and branch less than or equal
+# + aoblss - add one and branch less than
+# + bgtr,bleq,bneq,bnequ,beql,beqlu,bgeq,blss,bgtru,blequ
+# + bvc,bvs,bgequ,bcc,blssu,bcs - various branches, signed and unsigned
+# + bbs,bbc - branch on bit set/clea
+# + bbss,bbcs,bbsc,bbcc - branch on bit and set/clear
+# + bbssi,bbcci - branch on bit set and set/clear interlocked
+# + blbs, blbc - branch on low bit set/clear
+# + brb,brw - branch
+# + bsbb,bsbw - branch to subroutine.  PC is pushed on stack
+# + caseb,casew,casel - case statements in hardware
+# + jmp - jump
+# + jsb - jump to subroutine
+# + rsb - return from subroutine (equiv to jmp @(sp)+ but 1 byte shorter)
+# + sobgeq - subtract one and branch greater than or equal
+# + sobgtr - subtract one and branch greater than
 
 # procedure call instructions
-# callg - call with general arg list
-# calls - call with stack argument list
-# ret - return from procedure
+# + callg - call with general arg list
+# + calls - call with stack argument list
+# + ret - return from procedure
 
 # misc instructions
-# bicpsw,bispsw,bpt,bug,halt,index,movpsl,nop,popr,pushr
-# xfc - extdended func call, you can extend instr set on fly?
+# + bicpsw,bispsw,bpt,bug,halt,index,movpsl,nop,popr,pushr
+# + xfc - extdended func call, you can extend instr set on fly?
 
 # queue instructions
-# insqhi,insqti,insque,remqhi,remqti,remque,
+# + insqhi,insqti,insque,remqhi,remqti,remque,
 
 # floating point instructions
-# add,clr,cmp,cvt,div,emod,mneg,mov,mul
-# poly - evaluate polynomial!
-# sub,tst
+# + add,clr,cmp,cvt,div,emod,mneg,mov,mul
+# + poly - evaluate polynomial!
+# + sub,tst
 
 # character-string instructions
 #   r0-r1, r0-r3 or r0-r5 used
 #   all but movc may be implemented in software
-# cmpc3,cmpc5 - compare characters
+# + cmpc3,cmpc5 - compare characters
 #                string specified by r0,r1 compared to r3 (or r3,r4)
-# locc - locate character
-# matchc - match characters (search for substring)
-# movc - move character 
-# movtc - move translated characters
-# movtuc - move translated until character
-# scanc - scan characters (uses lookup table?)
-# skpc - skip character
-# spanc - span characters (uses lookup tapble?)
+# + locc - locate character
+# + matchc - match characters (search for substring)
+# + movc - move character
+# + movtc - move translated characters
+# + movtuc - move translated until character
+# + scanc - scan characters (uses lookup table?)
+# + skpc - skip character
+# + spanc - span characters (uses lookup tapble?)
 
 # CRC instructions
-# crc
+# + crc
 
 # Decimal-string instructions
-# addp,ashp,cmpp,cvtlp,cvtpl,cvtps,cvtpt,cvtps,cvttp,divp,movp
-# mulp,subp
-# edit,editpc - format strings for output (COBOL, PL/I, etc)
-# eo$adjust_input,eo$blank_zero,eo$end,eo$end_float,eo$fill,eo$float
-# eo$insert,eo$load,eo$move,eo$replace_sign,eo$_signif,eo$store_sign
+# + addp,ashp,cmpp,cvtlp,cvtpl,cvtps,cvtpt,cvtps,cvttp,divp,movp
+# + mulp,subp
+# + edit,editpc - format strings for output (COBOL, PL/I, etc)
+# + eo$adjust_input,eo$blank_zero,eo$end,eo$end_float,eo$fill,eo$float
+# + eo$insert,eo$load,eo$move,eo$replace_sign,eo$_signif,eo$store_sign
 
 # various os instructions, including task switching ones
 # ldpctx and svpctx that load/save process context
@@ -215,7 +215,7 @@
 
 
 
-	.globl _start	
+	.globl _start
 _start:
 	.align 1
 	.word 0x0			# this is the "entry mask"
@@ -243,7 +243,7 @@ _start:
 decompression_loop:
 	clrl	%r5		# reload the shift count
 	movb	(%r1)+,%r7	# load in a byte
-	
+
 test_flags:
 	cmpl	$logo_end, %r1 	# have we reached the end?
 	beql	done_logo  	# if so, exit
@@ -253,36 +253,36 @@ test_flags:
 
 offset_length:
 	movzbl 	(%r1)+,%r2  	# get match_length and match_position
-	movzbl  (%r1)+,%r11  	
+	movzbl  (%r1)+,%r11
 	ashl 	$8,%r11,%r11
 	bisb2	%r2,%r11    	# have to swap bytes, big-endian
-		
+
 	movzwl 	%r11,%r2	# copy to r2
 	    			# no need to mask r2, as we do it
 				# by default in output_loop
 
 
-	ashl 	$-(P_BITS),%r2,%r2	
+	ashl 	$-(P_BITS),%r2,%r2
 	addl2	$(THRESHOLD+1),%r2
-		
+
 output_loop:
 	bicw2 	$~(POSITION_MASK<<8+0xff),%r11  	# mask it
-	
+
 	movb 	text_buf(%r11), %r10	# load byte from text_buf[]
 	incl	%r11
-	
+
 store_byte:
 	movb	%r10,(%r3)+		# store it
-	
+
 	movb	%r10,text_buf(%r4)	# store also to text_buf[r]
 	incl 	%r4 			# r++
 	bicw2	$~(N-1),%r4		# mask r
 
 	acbb 	$1,$-1,%r2,output_loop	# repeat until k>j
-	
+
 	acbb	$7,$1,%r5,test_flags	# add to our bit offset and loop
 					# if not >7
-	
+
 	brb 	decompression_loop
 
 discrete_char:
@@ -298,14 +298,14 @@ done_logo:
 	movl	%r6,%r3			# move out_buffer to r3
 	bsbw	write_stdout		# print the logo
 
-	
+
 	#==========================
 	# PRINT VERSION
 	#==========================
-first_line:	
+first_line:
 	movl	%r6,%r11		# point to output buffer
 
-	moval	uname_info,%r7	
+	moval	uname_info,%r7
 	pushl   %r7   			# uname_struct
 	movzbl	$SYSCALL_UNAME,%r0	# uname syscall
 	calls	$1,syscall		# call syscall handler, 1 parm
@@ -318,7 +318,7 @@ first_line:
 
 	moval	U_RELEASE(%r7),%r5	# version from uname "2.4.1"
 	bsbb	strcat			# call strcat
-	
+
 	moval	b`COMP_OFFSET(%r9),%r5	# source is ", Compiled "
 	bsbb	strcat			# call strcat
 
@@ -326,7 +326,7 @@ first_line:
 	bsbb 	strcat			# call strcat
 
 	movw	$0x000a,(%r11)		# store linefeed and 0 on end
-	
+
 	bsbw	center_and_print	# center and print
 
 	#===============================
@@ -335,7 +335,7 @@ first_line:
 middle_line:
 
 	movl   %r6,%r11			# point to output buffer
-	
+
 	#=========
 	# Load /proc/cpuinfo into buffer
 	#=========
@@ -370,8 +370,8 @@ middle_line:
 strcat:
 	locc 	$0,$65535,(%r5)	# look for byte 0, looking at up to 64k chars
 				# r0=bytes remaining, r1=address located
-	
-strcat_know_size:	
+
+strcat_know_size:
 	subl3	%r5,%r1,%r0	# get the string length by subtracting
 
 
@@ -402,14 +402,14 @@ print_mhz:
 	#=========
 	# Chip Name
 	#=========
-chip_name:	
+chip_name:
 	moval	b`TYPE_OFFSET(%r9),%r5	# want to find "type"
 	bsbb	find_string		# get from cpuinfo file
-	
+
 	moval	b`PROC_OFFSET(%r9),%r5	# "Processor, "
 	bsbb	strcat			# call strcat
 
-	
+
 	#========
 	# RAM
 	#========
@@ -426,7 +426,7 @@ chip_name:
 
 	movl	%r1,%r5
 	bsbb	strcat 			# print value
-	
+
 	moval	b`RAM_OFFSET(%r9),%r5	# print 'M RAM, '
 	bsbb	strcat			# call strcat
 
@@ -447,13 +447,13 @@ chip_name:
 	#=================================
 third_line:
 	movl	%r6,%r11		# point to output buffer
-	
+
 	moval	b`U_NODENAME(%r7),%r5	# host name from uname()
 	bsbb    strcat			# call strcat
-	
+
 	bsbb	center_and_print	# center and print
-	
-	moval	b`COLORS_OFFSET(%r9),%r3	# restore colors	
+
+	moval	b`COLORS_OFFSET(%r9),%r3	# restore colors
 	bsbb	write_stdout
 
 
@@ -466,19 +466,18 @@ exit:
 	calls  $1,syscall		# call syscall handler, 1 parm
 
 
-	
 
 	#=================================
-	# FIND_STRING 
+	# FIND_STRING
 	#=================================
 	#   %r5 points to 4-char ascii string to look for
 	#   edi points at output buffer
 
-find_string:	
-	
+find_string:
+
 	# NOTE!  The matchc instruction is optional
 	#        you have to hack the code to turn it on in simh 3.7
-	
+
 	moval	disk_buffer,%r0		# look in cpuinfo buffer
 	matchc	$4,(%r5),$4096,(%r0)	# search for substring r5 (size 4)
 					# in string r0 (size 4096)
@@ -489,10 +488,10 @@ find_string:
 find_colon:
 	locc	$':',$65535,(%r5)	# find a colon
 	addl3	%r1,$2,%r5		# copy the pointer and skip space
-	
-store_loop:	 
+
+store_loop:
 	locc	$'\n',$65535,(%r5)	# find size
-	
+
 	brw	strcat_know_size
 
 
@@ -503,30 +502,30 @@ store_loop:
 	# %r11 has the end of the string
 
 center_and_print:
-	
-	
+
+
 	moval	ESCAPE_OFFSET(%r9),%r3	# we want to output ^[[
-	bsbb	write_stdout	
-	
+	bsbb	write_stdout
+
 	mnegl	%r6,%r3			# get length of string (end-begin)
 	addl2	%r11,%r3		# r3=%r11-%r3
 
-	movl	$80,%r1	
+	movl	$80,%r1
 
 	cmpb	%r3,%r1	    		# is it greater than 80?
 	bgeq	done_center		# if so, branch
 
 	subl3	%r3,%r1,%r3		# subtract size from 80
-	
+
 	ashl	$-1,%r3,%r2     	# then divide by 2
-	
+
 	bsbb	num_to_ascii		# print number of spaces
 	movl	%r1,%r3
 	bsbb	write_stdout
 
 	moval	C_OFFSET(%r9),%r3	# tack a 'C' on the end
 	bsbb	write_stdout
-	
+
 done_center:
 	movl	%r6,%r3			# fall through to write_stdout
 					# it will return for us
@@ -539,15 +538,15 @@ done_center:
 
 write_stdout:
 
-	
+
 	locc 	$0,$65535,(%r3)	# look for byte 0, looking at up to 64k chars
 				# r0=bytes remaining, r1=address located
-				
+
 	subl3	%r3,%r1,-(%sp)	# get the string length by subtracting
 				# and store it on the stack
 				# argument 3 (length)
 	pushl   %r3		# argument 2 (string)
-	pushl   $STDOUT	     	# argument 1 (stdout) 
+	pushl   $STDOUT	     	# argument 1 (stdout)
 	movzbl	$SYSCALL_WRITE,%r0
 	calls	$3,syscall	# call the syscall
 
@@ -558,16 +557,16 @@ write_stdout:
 	##############################
 	# r2 has number to convert
 	# result is pointed to by r1
-	
+
 num_to_ascii:
 	moval	ascii_buffer+8,%r1
 	clrl	%r3		# clear top half of the quadword
-	
+
 div_by_10:
 	ediv	$10,%r2,%r4,%r5	# divide r2 by 10, Q->%r4, R->%r5
 				# grrrr, the dividend is actually a quadword
 				# so r3 must be 0 or hilarity ensues
-	
+
 write_out:
 	addl2	$0x30,%r5	# convert to ASCII
 	movb	%r5,-(%r1)	# store to the buffer
@@ -581,7 +580,7 @@ write_out:
 	# syscall handler
 	#===============================
 	# syscall to call in r0
-	
+
 syscall:
 	.word   0x0			# entry mask
 	chmk	%r0			# do the syscall
