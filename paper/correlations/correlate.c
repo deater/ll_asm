b6c706926c1e6f892df9c0be2f8eb8c2640b24fc
Vince Weaver <vincent.weaver@maine.edu>
2017-05-23 14:47:10 -0400

    paper: update the correlations

--- paper/correlations/correlate.c
+++ paper/correlations/correlate.c
@@ -40,6 +40,37 @@ int read_data(char *filename, char *name) {
 
 }
 
+#define CONFIG_VLIW		0
+#define CONFIG_RISC		1
+#define CONFIG_EMBEDDED		2
+#define CONFIG_CISC		3
+#define CONFIG_SIXTEENBIT	4
+#define CONFIG_EIGHTBIT		5
+#define CONFIG_YEAR		6
+#define CONFIG_ENDIAN		7
+#define CONFIG_BITS		8
+#define CONFIG_MIN_INSTR	9
+#define CONFIG_MAX_INSTR	10
+#define CONFIG_NUM_ARGS		11
+#define CONFIG_GPRS		12
+#define CONFIG_ZERO_REG		13
+#define CONFIG_UNALIGNED	14
+#define CONFIG_AUTOINC		15
+#define CONFIG_HWDIV		16
+#define CONFIG_STATUSFLAG	17
+#define CONFIG_BRANCHDELAY	18
+#define CONFIG_PREDICATION	19
+#define CONFIG_FIRST_ADDRESS	20
+#define CONFIG_LOG_FIRST_ADDR	21
+#define MAX_CONFIG		22
+
+#define SIZE_FINDSTRING	0
+#define SIZE_LZSS		1
+#define SIZE_NUMASCII		2
+#define SIZE_STRCAT		3
+#define SIZE_OVERALL		4
+#define MAX_SIZE		5
+
 int main(int argc, char **argv) {
 
 	FILE *fff;
@@ -53,33 +84,8 @@ int main(int argc, char **argv) {
 
 	struct {
 		char name[BUFSIZ];
-		int vliw;
-		int risc;
-		int embedded;
-		int cisc;
-		int sixteenbit;
-		int eightbit;
-		int year;
-		int endian;
-		int bits;
-		int min_instr;
-		int max_instr;
-		int num_args;
-		int gprs;
-		int zero_reg;
-		int unaligned_mem;
-		int autoinc_addr;
-		double hw_div;
-		int status_flag;
-		int branch_delay;
-		int predication;
-		long long first_address;
-		double log_first_address;
-		int findstring_size;
-		int lzss_size;
-		int numascii_size;
-		int strcat_size;
-		int overall_size;
+		int config[MAX_CONFIG];
+		int size[MAX_SIZE];
 	} hw_info[MAX_ARCH];
 
 	dir=opendir("../configs");
@@ -87,6 +93,9 @@ int main(int argc, char **argv) {
 		fprintf(stderr,"Error opening ../configs\n");
 		exit(1);
 	}
+
+	long long temp_long;
+	double temp_double;
 
 	while(1) {
 		dir_file=readdir(dir);
@@ -105,63 +114,71 @@ int main(int argc, char **argv) {
 		result=fgets(string,BUFSIZ,fff);
 		sscanf(string,"%s",hw_info[arch].name);
 		result=fgets(string,BUFSIZ,fff);
-		sscanf(string,"%d",&hw_info[arch].vliw);
+		sscanf(string,"%d",&hw_info[arch].config[CONFIG_VLIW]);
 		result=fgets(string,BUFSIZ,fff);
-		sscanf(string,"\n%d",&hw_info[arch].risc);
+		sscanf(string,"\n%d",&hw_info[arch].config[CONFIG_RISC]);
 		result=fgets(string,BUFSIZ,fff);
-		sscanf(string,"\n%d",&hw_info[arch].embedded);
+		sscanf(string,"\n%d",&hw_info[arch].config[CONFIG_EMBEDDED]);
 		result=fgets(string,BUFSIZ,fff);
-		sscanf(string,"\n%d",&hw_info[arch].cisc);
+		sscanf(string,"\n%d",&hw_info[arch].config[CONFIG_CISC]);
 		result=fgets(string,BUFSIZ,fff);
-		sscanf(string,"\n%d",&hw_info[arch].sixteenbit);
+		sscanf(string,"\n%d",&hw_info[arch].config[CONFIG_SIXTEENBIT]);
 		result=fgets(string,BUFSIZ,fff);
-		sscanf(string,"\n%d",&hw_info[arch].eightbit);
+		sscanf(string,"\n%d",&hw_info[arch].config[CONFIG_EIGHTBIT]);
 		result=fgets(string,BUFSIZ,fff);
-		sscanf(string,"\n%d",&hw_info[arch].year);
+		sscanf(string,"\n%d",&hw_info[arch].config[CONFIG_YEAR]);
 		result=fgets(string,BUFSIZ,fff);
-		sscanf(string,"%d",&hw_info[arch].endian);
+		sscanf(string,"%d",&hw_info[arch].config[CONFIG_ENDIAN]);
 		result=fgets(string,BUFSIZ,fff);
-		sscanf(string,"%d",&hw_info[arch].bits);
+		sscanf(string,"%d",&hw_info[arch].config[CONFIG_BITS]);
 		result=fgets(string,BUFSIZ,fff);
-		sscanf(string,"%d",&hw_info[arch].min_instr);
+		sscanf(string,"%d",&hw_info[arch].config[CONFIG_MIN_INSTR]);
 		result=fgets(string,BUFSIZ,fff);
-		sscanf(string,"%d",&hw_info[arch].max_instr);
+		sscanf(string,"%d",&hw_info[arch].config[CONFIG_MAX_INSTR]);
 		result=fgets(string,BUFSIZ,fff);
-		sscanf(string,"%d",&hw_info[arch].num_args);
+		sscanf(string,"%d",&hw_info[arch].config[CONFIG_NUM_ARGS]);
 		result=fgets(string,BUFSIZ,fff);
-		sscanf(string,"%d",&hw_info[arch].gprs);
+		sscanf(string,"%d",&hw_info[arch].config[CONFIG_GPRS]);
 		result=fgets(string,BUFSIZ,fff);
-		sscanf(string,"%d",&hw_info[arch].zero_reg);
+		sscanf(string,"%d",&hw_info[arch].config[CONFIG_ZERO_REG]);
 		result=fgets(string,BUFSIZ,fff);
-		sscanf(string,"%d",&hw_info[arch].unaligned_mem);
+		sscanf(string,"%d",&hw_info[arch].config[CONFIG_UNALIGNED]);
 		result=fgets(string,BUFSIZ,fff);
-		sscanf(string,"%d",&hw_info[arch].autoinc_addr);
+		sscanf(string,"%d",&hw_info[arch].config[CONFIG_AUTOINC]);
 		result=fgets(string,BUFSIZ,fff);
-		sscanf(string,"%lf",&hw_info[arch].hw_div);
+		sscanf(string,"%lf",&temp_double);
+		hw_info[arch].config[CONFIG_HWDIV]=temp_double*10.0;
 		result=fgets(string,BUFSIZ,fff);
-		sscanf(string,"%d",&hw_info[arch].status_flag);
+		sscanf(string,"%d",&hw_info[arch].config[CONFIG_STATUSFLAG]);
 		result=fgets(string,BUFSIZ,fff);
-		sscanf(string,"%d",&hw_info[arch].branch_delay);
+		sscanf(string,"%d",&hw_info[arch].config[CONFIG_BRANCHDELAY]);
 		result=fgets(string,BUFSIZ,fff);
-		sscanf(string,"%d",&hw_info[arch].predication);
+		sscanf(string,"%d",&hw_info[arch].config[CONFIG_PREDICATION]);
 		result=fgets(string,BUFSIZ,fff);
-		sscanf(string,"%lf",&hw_info[arch].log_first_address);
+		sscanf(string,"%lf",&temp_double);
+		hw_info[arch].config[CONFIG_LOG_FIRST_ADDR]=(int)temp_double;
 		result=fgets(string,BUFSIZ,fff);
-		sscanf(string,"%llx",&hw_info[arch].first_address);
+		sscanf(string,"%llx",&temp_long);
+		hw_info[arch].config[CONFIG_FIRST_ADDRESS]=temp_long;
 
-		hw_info[arch].lzss_size=read_data("../data/data.lzss",
+		if (result==NULL) {
+			fprintf(stderr,"Error in result!\n");
+			exit(0);
+		}
+
+		hw_info[arch].size[SIZE_LZSS]=read_data("../data/data.lzss",
 						hw_info[arch].name);
 
-		hw_info[arch].findstring_size=read_data("../data/data.findstring",
+		hw_info[arch].size[SIZE_FINDSTRING]=read_data("../data/data.findstring",
 						hw_info[arch].name);
 
-		hw_info[arch].numascii_size=read_data("../data/data.num_ascii",
+		hw_info[arch].size[SIZE_NUMASCII]=read_data("../data/data.num_ascii",
 						hw_info[arch].name);
 
-		hw_info[arch].strcat_size=read_data("../data/data.strcat",
+		hw_info[arch].size[SIZE_STRCAT]=read_data("../data/data.strcat",
 						hw_info[arch].name);
 
-		hw_info[arch].overall_size=read_data("../data/data.overall",
+		hw_info[arch].size[SIZE_OVERALL]=read_data("../data/data.overall",
 						hw_info[arch].name);
 
 
@@ -177,25 +194,37 @@ int main(int argc, char **argv) {
 
 	closedir(dir);
 
+// TODO: print whole table at once
+// better yet, auto-generate table
+
+//	int config=CONFIG_MIN_INSTR;
+//	int size=SIZE_OVERALL;
+
+	int config=CONFIG_MAX_INSTR;
+	int size=SIZE_LZSS;
+
 	xbar=0.0; ybar=0.0;
 	for(i=0;i<arch;i++) {
-		xbar+=hw_info[i].min_instr;
-		ybar+=hw_info[i].overall_size;
+		xbar+=hw_info[i].config[config];
+		ybar+=hw_info[i].size[size];
 	}
 	xbar/=arch;
 	ybar/=arch;
 
 	sx=0.0; sy=0.0;
 	for(i=0;i<arch;i++) {
-		sx+=(hw_info[i].min_instr-xbar)*(hw_info[i].min_instr-xbar);
-		sy+=(hw_info[i].overall_size-ybar)*(hw_info[i].overall_size-ybar);
+		sx+=(hw_info[i].config[config]-xbar)*
+			(hw_info[i].config[config]-xbar);
+		sy+=(hw_info[i].size[size]-ybar)*
+			(hw_info[i].size[size]-ybar);
 	}
 	sx=sqrt(sx/arch);
 	sy=sqrt(sy/arch);
 
 	r=0.0;
 	for(i=0;i<arch;i++) {
-		 r+=((hw_info[i].min_instr-xbar)/sx) * ((hw_info[i].overall_size-ybar)/sy);
+		 r+=((hw_info[i].config[config]-xbar)/sx) *
+			((hw_info[i].size[size]-ybar)/sy);
 	}
 	r/=arch;
 
@@ -225,8 +254,8 @@ int main(int argc, char **argv) {
 	for(i=0;i<arch;i++) {
 
 		printf("%d %d (* %s *)\n",
-			hw_info[i].min_instr,
-			hw_info[i].overall_size,
+			hw_info[i].config[config],
+			hw_info[i].size[size],
 			hw_info[i].name);
 	}
 
